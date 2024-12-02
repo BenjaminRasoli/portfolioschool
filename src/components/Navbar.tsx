2d6821bcb2c21ebe57100bc122a96beb3593d9d4
BenjaminRasoli <Benjaminrasoli05@gmail.com>
2024-12-02 18:05:42 +0100

    update navbar, remove background gets added on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,22 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Navbar() {
   const [isOpen, setOpen] = useState<boolean>(false);
-  const [color, setColor] = useState("white");
-  const [githubColor, setGithubColor] = useState("white");
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -17,17 +31,28 @@ function Navbar() {
   });
 
   return (
-    <nav className="flex justify-between w-full items-center p-8 top-0 sticky bg-Main h-20 text-White">
-      <span>Benjamin | Rasoli</span>
-
+    <nav
+      className={`flex justify-between w-full items-center p-8 top-0 sticky h-20 ${
+        scrolled ? "bg-Main text-White" : "bg-transparent text-Black"
+      } transition-all duration-300`}
+      style={{
+        transition: "background-color 0.3s ease, color 0.3s ease",
+      }}
+    >
+      <h3 className={`text-h3 ${scrolled ? "text-White" : "text-Black"}`}>
+        Benjamin | Rasoli
+      </h3>
       <div
         className={`${
           isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
         } fixed top-0 left-0 bg-Main w-full h-screen transition-all duration-300 z-40`}
       >
-        <ul className="list-none flex flex-col pt-10 items-center h-full space-y-6">
+        <ul className="list-none flex flex-col pt-10 items-center h-full text-White space-y-6">
           {navLinks.map((navLink) => (
-            <li key={navLink.id} className="text-xl">
+            <h3
+              key={navLink.id}
+              className={`text-h3 ${scrolled ? "text-White" : "text-Black"}`}
+            >
               <a
                 href={`${navLink.id === "CV" ? cv : "#" + navLink.id}`}
                 download={navLink.id === "CV" && cv}
@@ -36,35 +61,31 @@ function Navbar() {
               >
                 {navLink.title}
               </a>
-            </li>
+            </h3>
           ))}
 
           <div className="flex gap-x-6 text-xl mt-6">
-            <a href="https://github.com/BenjaminRasoli" target="_blank">
-              <FaGithub
-                style={{ color: githubColor }}
-                onMouseEnter={() => setGithubColor("#26446b")}
-                onMouseLeave={() => setGithubColor("white")}
-              />
+            <a
+              href="https://github.com/BenjaminRasoli"
+              target="_blank"
+              className={`${scrolled ? "text-White" : "text-Black"}`}
+            >
+              <FaGithub size={30} />
             </a>
             <a
               href="https://www.linkedin.com/in/benjamin-rasoli-2948ab300"
               target="_blank"
+              className={`${scrolled ? "text-White" : "text-Black"}`}
             >
-              <FaLinkedin
-                style={{ color: color }}
-                onMouseEnter={() => setColor("#0A66C2")}
-                onMouseLeave={() => setColor("white")}
-              />
+              <FaLinkedin size={30} />
             </a>
           </div>
         </ul>
       </div>
-
-      <div className="hidden md:flex items-center gap-x-4">
+      <div className="hidden lg:flex items-center gap-x-4">
         {navLinks.map((navLink) => (
           <ul className="list-none" key={navLink.id}>
-            <li className="hover:underline">
+            <li className="hover:underline text-h3">
               <a
                 href={`${navLink.id === "CV" ? cv : "#" + navLink.id}`}
                 download={navLink.id === "CV" && cv}
@@ -77,28 +98,28 @@ function Navbar() {
         ))}
 
         <div className="flex gap-x-6 text-xl">
-          <a href="https://github.com/BenjaminRasoli" target="_blank">
-            <FaGithub
-              style={{ color: githubColor }}
-              onMouseEnter={() => setGithubColor("#26446b")}
-              onMouseLeave={() => setGithubColor("white")}
-            />
+          <a
+            href="https://github.com/BenjaminRasoli"
+            target="_blank"
+            className={`${scrolled ? "text-White" : "text-Black"}`}
+          >
+            <FaGithub size={30} />
           </a>
           <a
             href="https://www.linkedin.com/in/benjamin-rasoli-2948ab300"
             target="_blank"
+            className={`${scrolled ? "text-White" : "text-Black"}`}
           >
-            <FaLinkedin
-              style={{ color: color }}
-              onMouseEnter={() => setColor("#0A66C2")}
-              onMouseLeave={() => setColor("white")}
-            />
+            <FaLinkedin size={30} />
           </a>
         </div>
       </div>
-
-      <div className="md:hidden block z-50">
-        <Hamburger toggled={isOpen} toggle={setOpen} />
+      <div className="lg:hidden block z-50">
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          color={isOpen ? "White" : scrolled ? "White" : "Black"}
+        />
       </div>
     </nav>
   );
