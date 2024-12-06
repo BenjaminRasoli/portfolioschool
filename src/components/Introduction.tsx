393bc38766485b096608ae6f10ae8210690141a5
BenjaminRasoli <Benjaminrasoli05@gmail.com>
2024-12-06 18:40:50 +0100

    added container

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,8 +1,7 @@
-
 function Introduction() {
   return (
-    <div className="w-full min-h-[50vh] bg-Main text-White flex justify-center flex-col text-center items-center">
-      <div className="w-full md:max-w-4xl px-4">
+    <div className="p-container w-full min-h-[50vh] bg-Main text-White flex justify-center flex-col text-center items-center">
+      <div className="w-full md:max-w-2xl px-4">
         <h1 className="text-h1">Hi, I’m Matt. Nice to meet you.</h1>
         <p className="mt-4 text-lg leading-relaxed">
           Since beginning my journey as a freelance designer 12 years ago, I've
@@ -16,4 +15,4 @@ function Introduction() {
   );
 }
 
-export default Introduction
+export default Introduction;
