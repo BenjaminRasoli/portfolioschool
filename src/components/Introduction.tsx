fc200efa1462e3b39c6a4a43204f9af5f30d4124
BenjaminRasoli <Benjaminrasoli05@gmail.com>
2024-12-08 20:47:21 +0100

    added id so navlinks work

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,6 +1,6 @@
 function Introduction() {
   return (
-    <div className=" ssm:p-container w-full min-h-[50vh] bg-Main text-White flex justify-center flex-col text-center items-center">
+    <div id="about" className="scroll-m-6 ssm:p-container w-full min-h-[50vh] bg-Main text-White flex justify-center flex-col text-center items-center">
       <div className="w-full md:max-w-2xl px-4">
         <h1 className="text-h1">Hi, I’m Benjamin. Nice to meet you.</h1>
         <p className="mt-4 text-lg leading-relaxed">
