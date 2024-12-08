55fcd8b7b327f256096c4c244c69f28cc1656720
BenjaminRasoli <Benjaminrasoli05@gmail.com>
2024-12-08 19:00:37 +0100

    fixed responsive, edited text

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,14 +1,13 @@
 function Introduction() {
   return (
-    <div className="p-container w-full min-h-[50vh] bg-Main text-White flex justify-center flex-col text-center items-center">
+    <div className=" ssm:p-container w-full min-h-[50vh] bg-Main text-White flex justify-center flex-col text-center items-center">
       <div className="w-full md:max-w-2xl px-4">
-        <h1 className="text-h1">Hi, I’m Matt. Nice to meet you.</h1>
+        <h1 className="text-h1">Hi, I’m Benjamin. Nice to meet you.</h1>
         <p className="mt-4 text-lg leading-relaxed">
-          Since beginning my journey as a freelance designer 12 years ago, I've
-          done remote work for agencies, consulted for startups, and
-          collaborated with talented people to create digital products for both
-          business and consumer use. I'm quietly confident, naturally curious,
-          and perpetually working on improving my chops.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+          eligendi voluptas, quaerat quo doloremque, maxime illum rerum soluta
+          adipisci quae rem explicabo, ipsa magni asperiores quisquam? Nobis
+          fuga nulla illum?
         </p>
       </div>
     </div>
